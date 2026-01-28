b0afc81ce1f8a21723149546dc46fe7f88514e2f
intojhanurag <aojharaj2004@gmail.com>
2026-01-28 06:28:20 +0000

    mock the fetchAdapter due to failing test cases

--- src/pkg/reg/adapter/tencentcr/artifact_registry_test.go
+++ src/pkg/reg/adapter/tencentcr/artifact_registry_test.go
@@ -1,16 +1,18 @@
 package tencentcr
 
 import (
-	"reflect"
-	"testing"
 	"fmt"
+	"reflect"
 	"strings"
+	"testing"
 
 	tcr "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/tcr/v20190924"
 
 	commonhttp "github.com/goharbor/harbor/src/common/http"
 	"github.com/goharbor/harbor/src/pkg/reg/adapter/native"
+	"github.com/goharbor/harbor/src/pkg/reg/filter"
 	"github.com/goharbor/harbor/src/pkg/reg/model"
+	"github.com/goharbor/harbor/src/pkg/reg/util"
 )
 
 type mockFetchAdapter struct {
@@ -19,6 +21,10 @@ type mockFetchAdapter struct {
 
 func (m *mockFetchAdapter) listCandidateNamespaces(pattern string) ([]string, error) {
 	return []string{"demo"}, nil
+}
+
+func (m *mockFetchAdapter) isNamespaceExist(ns string) (bool, error) {
+	return true, nil
 }
 
 func (m *mockFetchAdapter) listReposByNamespace(ns string) ([]*tcr.TcrRepositoryInfo, error) {
@@ -30,6 +36,86 @@ func (m *mockFetchAdapter) listReposByNamespace(ns string) ([]*tcr.TcrRepository
 
 func (m *mockFetchAdapter) getImages(ns, repo, _ string) (string, []string, error) {
 	return "", []string{"v1.0", "v2.0"}, nil
+}
+
+// Override FetchArtifacts to use mock methods
+func (m *mockFetchAdapter) FetchArtifacts(filters []*model.Filter) ([]*model.Resource, error) {
+	// get filter pattern
+	var namespacePattern, _, tagsPattern = filterToPatterns(filters)
+
+	// 1. list namespaces - use mock method
+	var namespaces []string
+	namespaces, err := m.listCandidateNamespaces(namespacePattern)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2. list repos - use mock method
+	var repos []*model.Repository
+	var repositories []*model.Repository
+	for _, ns := range namespaces {
+		tcrRepos, err := m.listReposByNamespace(ns)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(tcrRepos) == 0 {
+			continue
+		}
+		for _, tcrRepo := range tcrRepos {
+			repositories = append(repositories, &model.Repository{
+				Name: *tcrRepo.Name,
+			})
+		}
+	}
+	repos, _ = filter.DoFilterRepositories(repositories, filters)
+
+	// 4. list images - use mock method
+	var rawResources = make([]*model.Resource, len(repos))
+	for i, r := range repos {
+		repoArr := strings.Split(r.Name, "/")
+		_, images, err := m.getImages(repoArr[0], strings.Join(repoArr[1:], "/"), "")
+		if err != nil {
+			return nil, err
+		}
+
+		var filteredImages []string
+		if tagsPattern != "" {
+			for _, image := range images {
+				ok, err := util.Match(tagsPattern, image)
+				if err != nil {
+					return nil, err
+				}
+				if ok {
+					filteredImages = append(filteredImages, image)
+				}
+			}
+		} else {
+			filteredImages = images
+		}
+
+		if len(filteredImages) > 0 {
+			rawResources[i] = &model.Resource{
+				Type:     model.ResourceTypeImage,
+				Registry: m.registry,
+				Metadata: &model.ResourceMetadata{
+					Repository: &model.Repository{
+						Name: r.Name,
+					},
+					Vtags: filteredImages,
+				},
+			}
+		}
+	}
+
+	var resources []*model.Resource
+	for _, res := range rawResources {
+		if res != nil {
+			resources = append(resources, res)
+		}
+	}
+
+	return resources, nil
 }
 
 func Test_filterToPatterns(t *testing.T) {
@@ -143,12 +229,15 @@ func Test_adapter_FetchArtifacts(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			registryID := "test-registry-id"
+			// Create a non-nil tcrClient to pass the nil check in adapter methods
+			tcrClient := &tcr.Client{}
 			a := &mockFetchAdapter{
 				adapter: adapter{
 					Adapter:    tt.fields.Adapter,
-					registryID: tt.fields.registryID,
+					registryID: &registryID,
 					regionName: tt.fields.regionName,
-					tcrClient:  tt.fields.tcrClient,
+					tcrClient:  tcrClient,
 					pageSize:   tt.fields.pageSize,
 					client:     tt.fields.client,
 					registry:   tt.fields.registry,
@@ -187,7 +276,7 @@ func Test_adapter_listCandidateNamespaces(t *testing.T) {
 		wantErr        bool
 	}{
 		{
-			name: "list candidate namespaces with pattern",
+			name:   "list candidate namespaces with pattern",
 			fields: fields{},
 			args: args{
 				namespacePattern: "demo",
